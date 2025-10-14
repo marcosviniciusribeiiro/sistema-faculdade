785dcdf6b15db14b64ec086eec460d13d256a4c0
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 12:27:02 -0300

    Create EnderecoRepository.java

---
+++ src/main/java/com/sistema/faculdade/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.sistema.faculdade.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema.faculdade.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> { }
