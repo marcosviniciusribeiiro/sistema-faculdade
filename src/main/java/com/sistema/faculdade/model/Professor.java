99c5ffb8069977dc2aa85a86bc2f2893e2073de5
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 10:28:43 -0300

    Create Professor.java

---
+++ src/main/java/com/sistema/faculdade/model/Professor.java
@@ -0,0 +1,48 @@
+package com.sistema.faculdade.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_professores")
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int matricula;
+	
+	private String nome;
+	
+	@ManyToMany(mappedBy = "professor")
+	private List<Disciplina> disciplinas = new ArrayList<>();
+	
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+}
