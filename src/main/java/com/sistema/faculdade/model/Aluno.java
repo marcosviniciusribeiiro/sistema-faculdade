1bf3c76303b777906fe8a35ffe465fb7e2fb0a77
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 10:48:55 -0300

    Update Aluno.java

--- src/main/java/com/sistema/faculdade/model/Aluno.java
+++ src/main/java/com/sistema/faculdade/model/Aluno.java
@@ -1,5 +1,6 @@
 package com.sistema.faculdade.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +16,8 @@ public class Aluno {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int matricula;
-
+	
+	@Column(nullable = false)
 	private String nome;
 	
 	@OneToOne
