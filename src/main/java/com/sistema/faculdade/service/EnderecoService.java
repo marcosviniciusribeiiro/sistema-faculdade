d20727e9fe46904567ac974bea5131fd903a37be
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:40:24 -0300

    Update EnderecoService.java

--- src/main/java/com/sistema/faculdade/service/EnderecoService.java
+++ src/main/java/com/sistema/faculdade/service/EnderecoService.java
@@ -2,6 +2,8 @@ package com.sistema.faculdade.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sistema.faculdade.dto.EnderecoDTO;
+import com.sistema.faculdade.mapper.EnderecoMapper;
 import com.sistema.faculdade.repository.EnderecoRepository;
 
 @Service
@@ -10,5 +12,13 @@ public class EnderecoService {
 	
 	public EnderecoService(EnderecoRepository enderecoRepository) {
 		this.enderecoRepository = enderecoRepository;
+	}
+	
+	public void salvarEndereco(EnderecoDTO dto) {
+		enderecoRepository.save(EnderecoMapper.toEntity(dto));
+	}
+	
+	public void deletarEndereco(Long id) {
+		enderecoRepository.deleteById(id);
 	}
 }
