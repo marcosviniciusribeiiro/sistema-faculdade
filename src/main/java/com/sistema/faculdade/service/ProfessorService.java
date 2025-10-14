ad3f88e766697fd695688e15a04e47fdce4fadd9
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:37:26 -0300

    Create ProfessorService.java

---
+++ src/main/java/com/sistema/faculdade/service/ProfessorService.java
@@ -0,0 +1,14 @@
+package com.sistema.faculdade.service;
+
+import org.springframework.stereotype.Service;
+
+import com.sistema.faculdade.repository.ProfessorRepository;
+
+@Service
+public class ProfessorService {
+	public ProfessorRepository professorRepository;
+	
+	public ProfessorService(ProfessorRepository professorRepository) {
+		this.professorRepository = professorRepository;
+	}
+}
