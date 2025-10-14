aab61c8bd274a8db66bc284ee57c65c16fd96404
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:40:27 -0300

    Update ProfessorService.java

--- src/main/java/com/sistema/faculdade/service/ProfessorService.java
+++ src/main/java/com/sistema/faculdade/service/ProfessorService.java
@@ -2,6 +2,8 @@ package com.sistema.faculdade.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sistema.faculdade.dto.ProfessorDTO;
+import com.sistema.faculdade.mapper.ProfessorMapper;
 import com.sistema.faculdade.repository.ProfessorRepository;
 
 @Service
@@ -10,5 +12,13 @@ public class ProfessorService {
 	
 	public ProfessorService(ProfessorRepository professorRepository) {
 		this.professorRepository = professorRepository;
+	}
+	
+	public void salvarProfessor(ProfessorDTO dto) {
+		professorRepository.save(ProfessorMapper.toEntity(dto));
+	}
+	
+	public void deletarProfessor(Long id) {
+		professorRepository.deleteById(id);
 	}
 }
