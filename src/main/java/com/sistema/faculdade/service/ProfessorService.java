4d6406df47d5aafb61469c05744379f3e796331b
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:49:14 -0300

    Update ProfessorService.java

--- src/main/java/com/sistema/faculdade/service/ProfessorService.java
+++ src/main/java/com/sistema/faculdade/service/ProfessorService.java
@@ -1,9 +1,12 @@
 package com.sistema.faculdade.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.sistema.faculdade.dto.ProfessorDTO;
 import com.sistema.faculdade.mapper.ProfessorMapper;
+import com.sistema.faculdade.model.Professor;
 import com.sistema.faculdade.repository.ProfessorRepository;
 
 @Service
@@ -16,6 +19,16 @@ public class ProfessorService {
 	
 	public void salvarProfessor(ProfessorDTO dto) {
 		professorRepository.save(ProfessorMapper.toEntity(dto));
+	}
+	
+	public List<ProfessorDTO> listarProfessores(){
+		return professorRepository.findAll().stream().map(ProfessorMapper::toDTO).toList();
+	}
+	
+	public ProfessorDTO buscarPorId(Long id) {
+		Professor p = professorRepository.findById(id)
+										 .orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+		return ProfessorMapper.toDTO(p);
 	}
 	
 	public void deletarProfessor(Long id) {
