55c8876002a59d9085908e4b7fd538be1c3f9c30
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 12:16:34 -0300

    Update Disciplina.java

--- src/main/java/com/sistema/faculdade/model/Disciplina.java
+++ src/main/java/com/sistema/faculdade/model/Disciplina.java
@@ -3,6 +3,7 @@ package com.sistema.faculdade.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Disciplina {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(nullable = false)
 	private String nome;
 	
 	@ManyToMany
