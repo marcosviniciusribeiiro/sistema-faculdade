f412754ad62aef855f9f06065341ea316343d0c7
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:33:57 -0300

    Create DisciplinaService.java

---
+++ src/main/java/com/sistema/faculdade/service/DisciplinaService.java
@@ -0,0 +1,37 @@
+package com.sistema.faculdade.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sistema.faculdade.dto.DisciplinaDTO;
+import com.sistema.faculdade.mapper.DisciplinaMapper;
+import com.sistema.faculdade.model.Disciplina;
+import com.sistema.faculdade.repository.DisciplinaRepository;
+
+@Service
+public class DisciplinaService {
+	public DisciplinaRepository disciplinaRepository;
+	
+	public DisciplinaService(DisciplinaRepository disciplinaRepository) {
+		this.disciplinaRepository = disciplinaRepository;
+	}
+	
+	public void salvarDisciplina(DisciplinaDTO dto) {
+		disciplinaRepository.save(DisciplinaMapper.toEntity(dto));
+	}
+	
+	public List<DisciplinaDTO> listarDisciplinas(){
+		return disciplinaRepository.findAll().stream().map(DisciplinaMapper::toDTO).toList();
+	}
+	
+	public DisciplinaDTO buscarPorId(Long id) {
+		Disciplina d = disciplinaRepository.findById(id)
+										   .orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+		return DisciplinaMapper.toDTO(d);
+	}
+	
+	public void deletarDisciplina(Long id) {
+		disciplinaRepository.deleteById(id);
+	}
+}
