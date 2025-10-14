0e6e947323fafd11c036be366a7ebca15ff1d4cc
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 10:28:19 -0300

    Create CursoDTO.java

---
+++ src/main/java/com/sistema/faculdade/dto/CursoDTO.java
@@ -0,0 +1,40 @@
+package com.sistema.faculdade.dto;
+
+import java.util.List;
+
+import com.sistema.faculdade.model.Aluno;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CursoDTO {
+	private int id;
+	
+	@NotBlank(message = "O nome do curso é obrigatório!")
+	private String nome;
+	
+	private List<Aluno> alunos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+}
