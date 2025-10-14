3b29dd10891b4e94e2ce947a2737e47fee135585
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 12:10:25 -0300

    Create ProfessorDTO.java

---
+++ src/main/java/com/sistema/faculdade/dto/ProfessorDTO.java
@@ -0,0 +1,40 @@
+package com.sistema.faculdade.dto;
+
+import java.util.List;
+
+import com.sistema.faculdade.model.Disciplina;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ProfessorDTO {
+	private int matricula;
+	
+	@NotBlank(message = "O nome do professor é obrigatório!")
+	private String nome;
+	
+	private List<Disciplina> disciplinas;
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+}
