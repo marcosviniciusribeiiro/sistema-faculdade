73025ed21c95249750b99bf88417ea91056d9fb4
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 14:49:10 -0300

    Update EnderecoService.java

--- src/main/java/com/sistema/faculdade/service/EnderecoService.java
+++ src/main/java/com/sistema/faculdade/service/EnderecoService.java
@@ -1,9 +1,12 @@
 package com.sistema.faculdade.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.sistema.faculdade.dto.EnderecoDTO;
 import com.sistema.faculdade.mapper.EnderecoMapper;
+import com.sistema.faculdade.model.Endereco;
 import com.sistema.faculdade.repository.EnderecoRepository;
 
 @Service
@@ -16,6 +19,16 @@ public class EnderecoService {
 	
 	public void salvarEndereco(EnderecoDTO dto) {
 		enderecoRepository.save(EnderecoMapper.toEntity(dto));
+	}
+	
+	public List<EnderecoDTO> listarEnderecos(){
+		return enderecoRepository.findAll().stream().map(EnderecoMapper::toDTO).toList();
+	}
+	
+	public EnderecoDTO buscarPorId(Long id) {
+		Endereco e = enderecoRepository.findById(id)
+									   .orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+		return EnderecoMapper.toDTO(e);
 	}
 	
 	public void deletarEndereco(Long id) {
