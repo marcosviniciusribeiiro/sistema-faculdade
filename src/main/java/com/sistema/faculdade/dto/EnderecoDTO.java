80a673e23ce12ed57caac4fe2d78249073f829b8
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 10:49:11 -0300

    Create EnderecoDTO.java

---
+++ src/main/java/com/sistema/faculdade/dto/EnderecoDTO.java
@@ -0,0 +1,80 @@
+package com.sistema.faculdade.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class EnderecoDTO {
+	private int id;
+	
+	@NotBlank(message = "O CEP é obrigatório!")
+	private String cep;
+	
+	@NotBlank(message = "A cidade é obrigatório!")
+	private String cidade;
+	
+	@NotBlank(message = "O estado é obrigatório!")
+	private String estado;
+	
+	@NotBlank(message = "A rua é obrigatória!")
+	private String rua;
+	
+	@NotBlank(message = "O número da casa é obrigatório!")
+	private int numero;
+	
+	private String complemento;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+}
