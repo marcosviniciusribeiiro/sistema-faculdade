5e30c70d4fdeec58caf8ba27badcf792b336c518
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 12:26:58 -0300

    Create DisciplinaRepository.java

---
+++ src/main/java/com/sistema/faculdade/repository/DisciplinaRepository.java
@@ -0,0 +1,7 @@
+package com.sistema.faculdade.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema.faculdade.model.Disciplina;
+
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> { }
