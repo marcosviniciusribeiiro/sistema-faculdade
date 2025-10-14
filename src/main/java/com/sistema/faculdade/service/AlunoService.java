c083825d1b8e4dd22e65e82366119938efa93649
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-14 12:42:38 -0300

    Update AlunoService.java

--- src/main/java/com/sistema/faculdade/service/AlunoService.java
+++ src/main/java/com/sistema/faculdade/service/AlunoService.java
@@ -1,7 +1,11 @@
 package com.sistema.faculdade.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.sistema.faculdade.dto.AlunoDTO;
+import com.sistema.faculdade.mapper.AlunoMapper;
 import com.sistema.faculdade.repository.AlunoRepository;
 
 @Service
@@ -10,5 +14,13 @@ public class AlunoService {
 	
 	public AlunoService(AlunoRepository alunoRepository) {
 		this.alunoRepository = alunoRepository;
+	}
+	
+	public void salvarAluno(AlunoDTO dto) {
+		alunoRepository.save(AlunoMapper.toEntity(dto));
+	}
+	
+	public List<AlunoDTO> listarAlunos(){
+		return alunoRepository.findAll().stream().map(AlunoMapper::toDTO).toList();
 	}
 }
