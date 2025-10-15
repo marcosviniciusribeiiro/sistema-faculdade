d9d1f1fc40eeb5d4889d19b4a5966a7e6d555bb0
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-15 10:13:47 -0300

    Update CursoController.java

--- src/main/java/com/sistema/faculdade/controller/CursoController.java
+++ src/main/java/com/sistema/faculdade/controller/CursoController.java
@@ -2,10 +2,15 @@ package com.sistema.faculdade.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sistema.faculdade.dto.CursoDTO;
 import com.sistema.faculdade.service.CursoService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CursoController {
@@ -21,5 +26,12 @@ public class CursoController {
 		return "form_curso";
 	}
 	
-	
+	@PostMapping("/cadastrar/curso")
+	public String salvarCurso(@ModelAttribute("cursoDTO") @Valid CursoDTO cursoDTO, BindingResult result) {
+		if(result.hasErrors()) {
+			return "form_curso";
+		}
+		service.salvarCurso(cursoDTO);
+		return "redirect:/cadastrar/curso";
+	}
 }
