ec8f32bca2b771d12d26ca2c38fe8579c071a8fb
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-15 11:27:34 -0300

    Update DisciplinaController.java

--- src/main/java/com/sistema/faculdade/controller/DisciplinaController.java
+++ src/main/java/com/sistema/faculdade/controller/DisciplinaController.java
@@ -1,8 +1,16 @@
 package com.sistema.faculdade.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sistema.faculdade.dto.DisciplinaDTO;
 import com.sistema.faculdade.service.DisciplinaService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class DisciplinaController {
@@ -10,5 +18,20 @@ public class DisciplinaController {
 	
 	public DisciplinaController(DisciplinaService service) {
 		this.service = service;
+	}
+	
+	@GetMapping("/cadastrar/disciplina")
+	public String formulario(Model model) {
+		model.addAttribute("disciplinaDTO", new DisciplinaDTO());
+		return "form_disciplina";
+	}
+	
+	@PostMapping("/cadastrar/disciplina")
+	public String salvarDisciplina(@ModelAttribute("disciplinaDTO") @Valid DisciplinaDTO disciplinaDTO, BindingResult result) {
+		if(result.hasErrors()){
+			return "form_disciplina";
+		}
+		service.salvarDisciplina(disciplinaDTO);
+		return "redirect:/cadastrar/disciplina";
 	}
 }
