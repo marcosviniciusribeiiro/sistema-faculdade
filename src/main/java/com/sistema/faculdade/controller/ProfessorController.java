ea34adbf3222dd63c3fc81738d41d0b007b3aac6
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-15 11:25:19 -0300

    Update ProfessorController.java

--- src/main/java/com/sistema/faculdade/controller/ProfessorController.java
+++ src/main/java/com/sistema/faculdade/controller/ProfessorController.java
@@ -1,8 +1,16 @@
 package com.sistema.faculdade.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sistema.faculdade.dto.ProfessorDTO;
 import com.sistema.faculdade.service.ProfessorService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProfessorController {
@@ -10,5 +18,20 @@ public class ProfessorController {
 	
 	public ProfessorController(ProfessorService service) {
 		this.service = service;
+	}
+	
+	@GetMapping("/cadastrar/professor")
+	public String formulario(Model model) {
+		model.addAttribute("professorDTO", new ProfessorDTO());
+		return "form_professor";
+	}
+	
+	@PostMapping("/cadastrar/professor")
+	public String salvarProfessor(@ModelAttribute("professorDTO") @Valid ProfessorDTO professorDTO, BindingResult result) {
+		if(result.hasErrors()) {
+			return "form_professor";
+		}
+		service.salvarProfessor(professorDTO);
+		return "redirect:/cadastrar/professor";
 	}
 }
