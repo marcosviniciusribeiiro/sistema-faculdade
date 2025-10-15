e47a76b475a793b3dfe073cc977ff1c5ef7c6283
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-15 12:03:05 -0300

    Update ProfessorController.java

--- src/main/java/com/sistema/faculdade/controller/ProfessorController.java
+++ src/main/java/com/sistema/faculdade/controller/ProfessorController.java
@@ -3,8 +3,10 @@ package com.sistema.faculdade.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sistema.faculdade.dto.ProfessorDTO;
@@ -34,13 +36,26 @@ public class ProfessorController {
 		service.salvarProfessor(professorDTO);
 		return "redirect:/cadastrar/professor";
 	}
-	//ListarProfessores ("/professores")
+
 	@GetMapping("/professores")
 	public String listarProfessores(Model model) {
 		model.addAttribute("professores", service.listarProfessores());
 		return "professores";
 	}
-	//EditarProfessor ("/professores/editar/{id}")
+	
+	@GetMapping("/professores/editar/{id}")
+	public String editarProfessor(Model model, @PathVariable Long id) {
+		ProfessorDTO dto = service.buscarPorId(id);
+		model.addAttribute("professorDTO", dto);
+		return "form_professor";
+	}
+	
 	//AtualizarProfessor ("/professores/atualizar/{id}")
+	
 	//ExcluirProfessor ("/professores/excluir/{id}")
+	@DeleteMapping("/professores/excluir/{id}")
+	public String excluirProfessor(@PathVariable Long id) {
+		service.excluirProfessor(id);
+		return "redirect:/professores";
+	}
 }
