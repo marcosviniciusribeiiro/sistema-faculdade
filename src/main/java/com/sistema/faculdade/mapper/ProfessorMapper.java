97e2607910a707483fc85f5d6a0d1a033beaafcd
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-15 11:49:13 -0300

    Update ProfessorMapper.java

--- src/main/java/com/sistema/faculdade/mapper/ProfessorMapper.java
+++ src/main/java/com/sistema/faculdade/mapper/ProfessorMapper.java
@@ -6,14 +6,14 @@ import com.sistema.faculdade.model.Professor;
 public class ProfessorMapper {
 	public static Professor toEntity(ProfessorDTO dto) {
 		Professor p = new Professor();
-		p.setMatricula(dto.getMatricula());
+		p.setId(dto.getId());
 		p.setNome(dto.getNome());
 		p.setDisciplinas(dto.getDisciplinas());
 		return p;
 	}
 	public static ProfessorDTO toDTO(Professor p) {
 		ProfessorDTO dto = new ProfessorDTO();
-		dto.setMatricula(p.getMatricula());
+		dto.setId(p.getId());
 		dto.setNome(p.getNome());
 		dto.setDisciplinas(p.getDisciplinas());
 		return dto;
