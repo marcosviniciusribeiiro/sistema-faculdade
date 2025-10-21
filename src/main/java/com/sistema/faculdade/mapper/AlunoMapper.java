05d41fac5a66ccf46fa664b337ed01ce9fb0a48b
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-21 10:36:29 -0300

    Update AlunoMapper.java

--- src/main/java/com/sistema/faculdade/mapper/AlunoMapper.java
+++ src/main/java/com/sistema/faculdade/mapper/AlunoMapper.java
@@ -5,19 +5,18 @@ import com.sistema.faculdade.model.Aluno;
 
 public class AlunoMapper {
 	public static Aluno toEntity(AlunoDTO dto) {
-		Aluno a = new Aluno();
-		a.setId(dto.getId());
-		a.setNome(dto.getNome());
-		a.setEndereco(dto.getEndereco());
-		a.setCurso(dto.getCurso());
-		return a;
+		Aluno aluno = new Aluno();
+		aluno.setId(dto.getId());
+		aluno.setNome(dto.getNome());
+		aluno.setEndereco(EnderecoMapper.toEntity(dto.getEndereco()));
+		return aluno;
 	}
-	public static AlunoDTO toDTO(Aluno a) {
+	public static AlunoDTO toDTO(Aluno aluno) {
 		AlunoDTO dto = new AlunoDTO();
-		dto.setId(a.getId());
-		dto.setNome(a.getNome());
-		dto.setEndereco(a.getEndereco());
-		dto.setCurso(a.getCurso());
+		dto.setId(aluno.getId());
+		dto.setNome(aluno.getNome());
+		dto.setEndereco(EnderecoMapper.toDTO(aluno.getEndereco()));
+		dto.setCursoId(aluno.getCurso().getId());
 		return dto;
 	}
 }
