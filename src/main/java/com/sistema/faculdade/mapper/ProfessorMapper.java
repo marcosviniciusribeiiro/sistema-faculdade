2ef7ee9364a422f2161459798f7a95ab03d24999
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-21 12:05:52 -0300

    Update ProfessorMapper.java

--- src/main/java/com/sistema/faculdade/mapper/ProfessorMapper.java
+++ src/main/java/com/sistema/faculdade/mapper/ProfessorMapper.java
@@ -1,21 +1,24 @@
 package com.sistema.faculdade.mapper;
 
+import java.util.List;
+
 import com.sistema.faculdade.dto.ProfessorDTO;
+import com.sistema.faculdade.model.Disciplina;
 import com.sistema.faculdade.model.Professor;
 
 public class ProfessorMapper {
-	public static Professor toEntity(ProfessorDTO dto) {
+	public static Professor toEntity(ProfessorDTO dto, List<Disciplina> disciplinas) {
 		Professor p = new Professor();
 		p.setId(dto.getId());
 		p.setNome(dto.getNome());
-		p.setDisciplinas(dto.getDisciplinas());
+		p.setDisciplinas(disciplinas);
 		return p;
 	}
 	public static ProfessorDTO toDTO(Professor p) {
 		ProfessorDTO dto = new ProfessorDTO();
 		dto.setId(p.getId());
 		dto.setNome(p.getNome());
-		dto.setDisciplinas(p.getDisciplinas());
+		dto.setDisciplinasIds(p.getDisciplinas().stream().map(Disciplina::getId).toList());
 		return dto;
 	}
 }
