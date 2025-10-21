11a906ef6a976aa0986e76df94435e7b7c07cb88
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-21 13:23:20 -0300

    Update EnderecoController.java

--- src/main/java/com/sistema/faculdade/controller/EnderecoController.java
+++ src/main/java/com/sistema/faculdade/controller/EnderecoController.java
@@ -17,10 +17,10 @@ import jakarta.validation.Valid;
 
 @Controller
 public class EnderecoController {
-	public EnderecoService service;
+	private final EnderecoService enderecoService;
 	
-	public EnderecoController(EnderecoService service) {
-		this.service = service;
+	public EnderecoController(EnderecoService enderecoService) {
+		this.enderecoService = enderecoService;
 	}
 	
 	@GetMapping("/cadastrar/endereco")
@@ -36,19 +36,19 @@ public class EnderecoController {
 		if(result.hasErrors()) {
 			return "endereco_form";
 		}
-		service.salvarEndereco(enderecoDTO);
+		enderecoService.salvarEndereco(enderecoDTO);
 		return "redirect:/cadastrar/endereco";
 	}
 	
 	@GetMapping("/enderecos")
 	public String listarEnderecos(Model model) {
-		model.addAttribute("enderecos", service.listarEnderecos());
+		model.addAttribute("enderecos", enderecoService.listarEnderecos());
 		return "enderecos";
 	}
 	
 	@GetMapping("/enderecos/editar/{id}")
 	public String editarEndereco(@PathVariable Long id, Model model) {
-		EnderecoDTO dto = service.buscarPorId(id);
+		EnderecoDTO dto = enderecoService.buscarPorId(id);
 		model.addAttribute("enderecoDTO", dto);
 		return "endereco_form";
 	}
@@ -56,13 +56,13 @@ public class EnderecoController {
 	@PutMapping("/enderecos/atualizar/{id}")
 	public String atualizarEndereco(@ModelAttribute EnderecoDTO enderecoDTO, @PathVariable Long id) {
 		enderecoDTO.setId(id);
-		service.salvarEndereco(enderecoDTO);
+		enderecoService.salvarEndereco(enderecoDTO);
 		return "redirect:/enderecos";	
 	}
 	
 	@DeleteMapping("/enderecos/excluir/{id}")
 	public String excluirEndereco(@PathVariable Long id) {
-		service.excluirEndereco(id);
+		enderecoService.excluirEndereco(id);
 		return "redirect:/enderecos";
 	}
 }
