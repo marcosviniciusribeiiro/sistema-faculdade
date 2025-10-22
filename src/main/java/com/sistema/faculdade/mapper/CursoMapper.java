610223ddbfeb6005a7f0d407b9f697c83f4a02c4
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-21 22:11:36 -0300

    Update CursoMapper.java

--- src/main/java/com/sistema/faculdade/mapper/CursoMapper.java
+++ src/main/java/com/sistema/faculdade/mapper/CursoMapper.java
@@ -3,15 +3,16 @@ package com.sistema.faculdade.mapper;
 import java.util.List;
 
 import com.sistema.faculdade.dto.CursoDTO;
+import com.sistema.faculdade.model.Aluno;
 import com.sistema.faculdade.model.Curso;
 import com.sistema.faculdade.model.Disciplina;
 
 public class CursoMapper {
-	public static Curso toEntity(CursoDTO dto, List<Disciplina> disciplinas) {
+	public static Curso toEntity(CursoDTO dto, List<Disciplina> disciplinas, List<Aluno> alunos) {
 		Curso curso = new Curso();
 		curso.setId(dto.getId());
 		curso.setNome(dto.getNome());
-		curso.setAlunos(dto.getAlunos());
+		curso.setAlunos(alunos);
 		curso.setDisciplinas_curso(disciplinas);
 		return curso;
 	}
@@ -20,7 +21,7 @@ public class CursoMapper {
 		CursoDTO dto = new CursoDTO();
 		dto.setId(curso.getId());
 		dto.setNome(curso.getNome());
-		dto.setAlunos(curso.getAlunos());
+		dto.setAlunosIds(curso.getAlunos().stream().map(Aluno::getId).toList());
 		dto.setDisciplinasIds(curso.getDisciplinas_curso().stream().map(Disciplina::getId).toList());
 		return dto;
 	}
